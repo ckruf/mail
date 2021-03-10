b9e0e640c3e8d6ce4aa74a574dae8051dd849c1b
ckruf <christian.kruf@pwc.com>
2021-03-10 22:48:51 +0100

    All features implemented, minor bugs, code currently a little ugly

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -10,7 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
 });
 
-function compose_email() {
+//This function shows the email composition view and hides the other view.
+//The function takes two optional arguments - reply and id. 
+//If reply has a value of 'true', the user is replying to an email, and so certain fields are pre-filled. 'id' is then the id of the email they're replying to.
+function compose_email(reply, id) {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
@@ -21,6 +24,17 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  if (reply === true)
+  {
+    fetch(`/emails/${id}`)
+    .then(response => response.json())
+    .then(email => {
+      document.querySelector('#compose-recipients').value  = email.sender;
+      document.querySelector('#compose-subject').value = `RE: ${email.subject}`;
+      document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote:\n ${email.body}`;
+    })
+  }
 
   //Call the 'send_email' function when the submit button is clicked
   document.querySelector('#send').addEventListener('click', send_email);
@@ -39,10 +53,11 @@ function send_email() {
   .then(result => {
     console.log(result);
   });
-  load_mailbox(sent);
-  return false;
+  load_mailbox('sent');
 }
 
+//This function shows the mailbox view, hides other views and fetches the emails via an API request
+//It then calls the 'add_email' function on each of the fetched emails. That function then adds the emails to the DOM, into the emails-view div.
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
@@ -51,38 +66,46 @@ function load_mailbox(mailbox) {
   document.querySelector('#single-email-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3 id="mailboxname">${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#emails-view').innerHTML = `<h3 id="mailboxname">${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><div id="messages"></div>`;
 
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
     emails.forEach(function (emails) {
-      add_email(emails, mailbox)
+      if (mailbox === 'archive')
+      {
+        if (emails.archived === true)
+        {
+          add_email(emails, mailbox)
+        }
+      }
+      else
+      {
+        add_email(emails, mailbox)
+      }
     });
   })
 }
 
-function show_email(id) {
+//Show view of a single email, when it is clicked in a mailbox
+function show_email(id, mailbox) {
+
+  //Clear the 'emailmeta' div, in case a previous email has been loaded
+  document.querySelector('#emailmeta').innerHTML = '';
+  //Clear the 'buttons' div, so that buttons from previously loaded emails don't show up
+  document.querySelector('#buttons').innerHTML = '';
+  
   //show the single email view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#single-email-view').style.display = 'block';
-
-  // create a div containing the sender, receiver, subject and time
-  const emailmeta = document.createElement('div');
-  emailmeta.id = 'emailmeta';
-  document.querySelector('#single-email-view').append(emailmeta);
-
-  const separator = document.createElement('hr');
-  document.querySelector('#single-email-view').append(separator);
-
 
   const msgsender = document.createElement('div');
   msgsender.id = 'msgsender';
   
   const msgreceiver = document.createElement('div');
   msgreceiver.id = 'msgreceiver';
-  
+
   const msgsubject = document.createElement('div');
   msgsubject.id = 'msgsubject'
   
@@ -94,10 +117,38 @@ function show_email(id) {
   emailmeta.append(msgsubject);
   emailmeta.append(msgtime);
 
-  // create a div containing the body of the message
-  const emailbody = document.createElement('div');
-  emailbody.id = 'emailbody';
-  document.querySelector('#single-email-view').append(emailbody);
+  if (mailbox === 'inbox')
+  {
+    const replybtn = document.createElement('button');
+    replybtn.id = 'replybtn';
+    replybtn.className = 'btn btn-primary';
+    replybtn.innerHTML = 'Reply';
+    replybtn.onclick = function() {compose_email(true, id)};
+    document.querySelector('#buttons').append(replybtn);
+
+    const archivebtn = document.createElement('button');
+    archivebtn.id = 'archivebtn';
+    archivebtn.className = 'btn btn-primary';
+    archivebtn.innerHTML = 'Archive';
+    archivebtn.onclick = function() {archive_email(id)};
+    document.querySelector('#buttons').append(archivebtn);
+
+    fetch(`/emails/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        read: true
+      })
+    })
+  }
+  else if (mailbox === 'archive')
+  {
+    const unarchivebtn = document.createElement('button');
+    unarchivebtn.id = 'unarchivebtn';
+    unarchivebtn.className = 'btn btn-primary';
+    unarchivebtn.innerHTML = 'Unarchive';
+    unarchivebtn.onclick = function() {unarchive_email(id)};
+    document.querySelector('#buttons').append(unarchivebtn);
+  }
 
   fetch(`/emails/${id}`)
   .then(response => response.json())
@@ -109,47 +160,73 @@ function show_email(id) {
     emailbody.innerHTML = email.body;
   })
 
-  
-
 }
 
+//This gets called on each email 
 function add_email(contents, mailbox) {
   
-  //create div that contains the emails message. Give it a class of row, which will contain the various columns
+  //create div that contains the email message. Give it a class of row, which will contain the various columns
   const emailmsg = document.createElement('div');
   emailmsg.className = 'emailmsg row';
   emailmsg.id = `msg${contents.id}`;
-  document.querySelector('#emails-view').append(emailmsg);
-  emailmsg.onclick = function() {show_email(contents.id)};
+  document.querySelector('#messages').append(emailmsg);
+  emailmsg.onclick = function() {show_email(contents.id, mailbox)};
   
   //some logic about what properties of the email to display, depending on the mailbox the user is viewing
   //for example, you do not want the email receiver to appear in your inbox - since it is obviously you. Vice versa for 'sent' mailbox. 
-  if (mailbox === 'inbox') 
+  if (mailbox === 'inbox' || mailbox === 'archive') 
   {
     const msgsender = document.createElement('div');
-    msgsender.className = 'msgsender col';
+    msgsender.className = 'msgsender col-3';
     msgsender.innerHTML = contents.sender;
     document.querySelector(`#msg${contents.id}`).append(msgsender);
   }
   else if (mailbox === 'sent')
   {
     const msgreceiver = document.createElement('div');
-    msgreceiver.className = 'msgreceiver col';
+    msgreceiver.className = 'msgreceiver col-3';
     msgreceiver.innerHTML = contents.recipients;
     document.querySelector(`#msg${contents.id}`).append(msgreceiver);
   }
-  else if (mailbox === 'archived')
+
+  if (contents.read === false)
   {
-    //to do
+    emailmsg.style.backgroundColor = 'white';
+  }
+  else if (contents.read === true)
+  {
+    emailmsg.style.backgroundColor = 'WhiteSmoke';
   }
   
   const msgsubject = document.createElement('div');
-  msgsubject.className = 'msgsubject col';
+  msgsubject.className = 'msgsubject col-6';
   msgsubject.innerHTML = contents.subject;
   document.querySelector(`#msg${contents.id}`).append(msgsubject);
 
   const msgtime = document.createElement('div');
-  msgtime.className = 'msgtime col';
+  msgtime.className = 'msgtime col-3';
   msgtime.innerHTML = contents.timestamp;
   document.querySelector(`#msg${contents.id}`).append(msgtime); 
+}
+
+function archive_email(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: true
+    })
+  })
+
+  load_mailbox('inbox');
+}
+
+function unarchive_email(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: false
+    })
+  })
+
+  load_mailbox('inbox');
 }
